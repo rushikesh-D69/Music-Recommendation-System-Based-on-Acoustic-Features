ebaafa173b4e112c8e91834511ad5980898f5c4e
Chandan Sai Pavan Padala <chandansaipavanp@gmail.com>
2025-04-25 00:44:26 +0530

    Update front.py

--- front.py
+++ front.py
@@ -1,6 +1,6 @@
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"  # Use CPU only
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"   # Suppress TensorFlow logs: 0 = all logs, 1 = filter INFO, 2 = filter WARNING, 3 = filter ERROR
+#os.environ["CUDA_VISIBLE_DEVICES"] = "-1"  # Use CPU only
+#os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"   # Suppress TensorFlow logs: 0 = all logs, 1 = filter INFO, 2 = filter WARNING, 3 = filter ERROR
 
 import streamlit as st
 import numpy as np
